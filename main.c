151a90cad290ea19836c019482a3bb6bb75da0a1
Luis Eduardo Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-03-01 07:06:46 +0000

    Correcciones en los comentarios

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 
 /**
- * AUTORES:                                                    FECHA: 26-02-2023
- * - Luis Eduardo Galindo Amaya -
- * - Jorge Omar Torres Sosa - 1270900
- * - Mariano Perez Piña -
+ * AUTORES:                                                   FECHA: 26-02-2023
+ * 1274895 - Luis Eduardo Galindo Amaya
+ * 1270900 - Jorge Omar Torres Sosa
+ * - Mariano Perez Piña
  *
- * DESCRIPCIÓN:
- * M2.1 Ejercicio de ordenamiento básico. Escribe un programa que capture los
- * gastos de una persona y que calcule cuánto ha gastado en total.
+ * DESCRIPCIÓN: M2.1 Ejercicio de ordenamiento básico.
+ * Escribe un programa que capture los gastos de una persona y que calcule
+ * cuánto ha gastado en total.
  */
 
 #include "linkedList.h"
@@ -20,14 +20,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
 int main(void)
 {
   LinkedList pagosFijos;
   LinkedList entretenimiento;
   LinkedList comida;
-  
+
   linkedListInit(&pagosFijos);
   linkedListInit(&entretenimiento);
   linkedListInit(&comida);
@@ -42,86 +40,10 @@ int main(void)
 
   bubbleSort(&pagosFijos);
   printLista(&pagosFijos);
-  /* mostrar los elementos */
-  // printf("--- Elementos 1 ---\n");
-  // for (int i = 0; i < len(&lista); i++) {
-  //   display(get(&lista, i));
-  //   printf("\n");
-  // }
 
-  // printf("\n");
-
-  // /* copiar un elemento y ponerlo al final */
-  // LinkedListNode *boleto = get(&lista, 0);
-
-  // /* eliminar el boleto para morius de la lista */
-  // delete (&lista, 0);
-
-  // printf("--- Elementos 2 ---\n");
-  // for (int i = 0; i < len(&lista); i++) {
-  //   display(get(&lista, i));
-  //   printf("\n");
-  // }
-
-  // printf("\n");
-
-  // /* agregar un retiro */
-  // append(&lista, CATEGORY_FIXED_PAYMENT, "Retiro", 10000);
-
-  // printf("--- Elementos 3 ---\n");
-  // for (int i = 0; i < len(&lista); i++) {
-  //   display(get(&lista, i));
-  //   printf("\n");
-  // }
-  // printf("\n");
-
-  // /* insertar el boleto a la mitad */
-  // insert(&lista, boleto, 1);
-  // printf("--- Elementos 4 ---\n");
-  // for (int i = 0; i < len(&lista); i++) {
-  //   display(get(&lista, i));
-  //   printf("\n");
-  // }
-  // printf("\n");
-
-  // /* crear un nodo random e insertarlo */
-  // LinkedListNode *bar =
-  //     linkedListNodeCreate(CATEGORY_FIXED_PAYMENT, "Otro retiro", 100);
-  // insert(&lista, bar, len(&lista)); /* insertar al final */
-
-  // printf("--- Elementos 5 ---\n");
-  // for (int i = 0; i < len(&lista); i++) {
-  //   display(get(&lista, i));
-  //   printf("\n");
-  // }
-  // printf("\n");
-
-  /* liberar la memoria */
   linkedListFree(&pagosFijos);
   linkedListFree(&entretenimiento);
   linkedListFree(&comida);
 
   return 0;
 }
-
-// Funciones de la lista
-
-
-
-// Funciones de ordenamiento
-
-
-
-// for (int i = 0; i+1 < len(lista); i++)
-//   {
-//     if(strcmp(get(lista, i)->concept, get(lista, i+1)->concept) > 0)
-//     {
-
-//       printf("%s es menor que %s", get(lista, i+1)->concept, get(lista, i)->concept);
-//     }
-//     else
-//     {
-//       printf("%s es mayor que %s", get(lista, i+1)->concept, get(lista, i)->concept);
-//     }
-//     printf("\n");
-//   }  
