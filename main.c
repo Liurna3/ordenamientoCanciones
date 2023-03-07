5645c87760adb4b72d377589e9e1de2588f908fc
Luis Eduardo Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-03-07 06:51:45 +0000

    menu integrado en main

--- main.c
+++ main.c
@@ -19,37 +19,83 @@
 
 int main(void)
 {
-    // menu();   
-    // char concepto[STRING_LENGHT] = {0};
-    // float monto = 0;
-    // int op = 1;
-    // int tipo = 0;
-
     // Creando listas
+    int op;
+    char nombre[STRING_LENGHT];
+    char album[STRING_LENGHT];
+    char artista[STRING_LENGHT];
     LinkedList canciones;
 
     // Inicializando listas
     linkedListInit(&canciones);
 
-    append(&canciones, "El problema", "Un verano sin ti", "Bad Bunny");
+    do
+    {
+        printf(
+            "\n"
+            "--- PLAYLIST DE CANCIONES ---\n"
+            " [1] CAPTURAR CANCION\n"
+            " [2] MOSTRAR PLAYLIST ORDENADA POR CANCION\n"
+            " [3] MOSTRAR PLAYLIST ORDENADA POR ALBUM\n"
+            " [4] MOSTRAR PLAYLIST ORDENADA POR ARTISTA\n"
+            " [0] SALIR\n"
+            "-------------------------------\n"
+            "> ");
+
+        scanf("%d", &op);
+
+        switch (op)
+        {
+        case 1:
+            printf("--- CAPTURAR CANCION ----\n");
+            printf("NOMBRE: ");
+            stdinFlush();
+            getString(nombre, STRING_LENGHT);
+            printf("ARTISTA: ");
+            getString(artista, STRING_LENGHT);
+            printf("ALBUM: ");
+            getString(album, STRING_LENGHT);
+            printf("---------------------------\n");
+
+            // aquí va para crear el nodo
+            append(&canciones, nombre, album, artista);
+            break;
+
+        case 2:
+            // ordenada por cancion
+            mergeSort(&canciones);
+            printLista(&canciones);
+            break;
+
+        case 3:
+            quickSort(&canciones);
+            printLista(&canciones);
+            break;
+
+        case 4:
+            // por artista
+            shellSort(&canciones);
+            printLista(&canciones);
+            break;
+
+        case 0:
+            printf("Adios!!\n");
+            linkedListFree(&canciones);
+            break;
+
+        default:
+            printf("\nNo existe esa opcion!!\n");
+            break;
+        }
+
+    } while (op);
+
+    /* append(&canciones, "El problema", "Un verano sin ti", "Bad Bunny");
     append(&canciones, "Trastorno bipoalar", "Trastorno bipolar", "Porta");
     append(&canciones, "Apagado o Fuera de Cobertura", "Energia", "El Chojin");
     append(&canciones, "Un dia en Suburbia", "Un dia en Suburbia", "Nach");
     append(&canciones, "Meteorito", "Antonimo", "Rayden");
     append(&canciones, "Jeremias 17:5", "Muerte", "Canserbero");
-
-    mergeSort(&canciones);
-    printLista(&canciones);
-
-    printf("------------------\n");
-    shellSort(&canciones);
-    printLista(&canciones);
-
-    printf("------------------\n");
-    quickSort(&canciones);
-    printLista(&canciones);
-
-    linkedListFree(&canciones);
 
     return 0;
 }
